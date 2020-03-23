3a4892ba8de36a07611644efe4c034ff1a72ae69
bperez2210 <bperez2210@gmail.com>
2020-03-22 18:06:27 -0600

    Home & Navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './Pages/Main/app.component';
+import { HomeComponent } from './Pages/home/home.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { RouterModule, Routes } from '@angular/router';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+
+const appRoutes: Routes = [
+  { path: '', component: AppComponent },
+  { path: 'Home', component: HomeComponent }];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FooterComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
